863c62f420a22498c7db47cd8c8801ccb4d360de
Martin Gontovnikas <martin@gonto.com.ar>
2011-10-21 17:34:33 -0300

    Added logged in

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,9 +1,15 @@
 package controllers;
+
+import models.User;
  
 public class Security extends Secure.Security {
     
     static boolean authentify(final String username, final String password) {
         return true;
     }    
+    
+    public static User loggedIn() {
+        return User.find("byMail", Security.connected()).first();
+    }
     
 }
