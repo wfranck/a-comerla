3370d8c25407d9603ddd065b604019c5b68769f3
Martin Gontovnikas <gonto@zauberlabs.com>
2012-01-10 14:36:30 -0300

    Cambiado el layout de security

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -7,6 +7,9 @@ import controllers.securesocial.SecureSocial;
 public class Security {
     
     public static User connected() {
+        if (SecureSocial.getCurrentUser() == null) {
+            return null;
+        }
         return UserSocialInformation.findByUserId(SecureSocial.getCurrentUser().id).user;
     }
 
